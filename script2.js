85dd015f49760cb9875c1e99e1e8d0e377fa5e47
Emma <emma.cotonat@gmail.com>
2025-10-31 01:28:16 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,8 +1,1 @@
-function updateDateTime(){
-  const now = new Date();
-  const date = now.toLocaleDateString('en-GB'); // format dd/mm/yyyy
-  const time = now.toLocaleTimeString('en-GB', { hour12: false });
-  document.getElementById('datetime').textContent = `${date}, ${time}`;
-}
-setInterval(updateDateTime, 1000);
-updateDateTime();
+
